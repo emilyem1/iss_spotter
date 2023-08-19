ca6701f30bb928d407c96734a362cb09e2ca679e
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-18 23:13:53 -0700

    adding fetchCoordsByIp callback

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+const ipAddress = '174.7.121.62'
+// const ipAddress = '42' to check error message is working 
+
+fetchCoordsByIP(ipAddress, (error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Coords here:' , ip);
+});
 
 // Testing complete!
 /* fetchMyIP((error, ip) => {
