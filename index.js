ddeb2a23e1c8e5e48f80da242fd96f855808d645
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-18 23:17:42 -0700

    commenting out the test for functions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // index.js
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-const ipAddress = '174.7.121.62'
-// const ipAddress = '42' to check error message is working 
+// Test for fetchCoordsByIp
+/*
+const ipAddress = '174.7.121.62';
+const ipAddress = '42' to check error message is working
 
 fetchCoordsByIP(ipAddress, (error, ip) => {
   if (error) {
@@ -10,9 +12,9 @@ fetchCoordsByIP(ipAddress, (error, ip) => {
     return;
   }
   console.log('It worked! Coords here:' , ip);
-});
+}); */
 
-// Testing complete!
+// Testing for fetchMyIp
 /* fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
