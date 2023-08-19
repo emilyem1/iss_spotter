d6723a5ff95e0b19529d4a6616b3abca158a6338
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-18 22:38:01 -0700

    commenting out test callback

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // index.js
 const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// Testing complete!
+/* fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned IP:' , ip);
-});
+}); */
